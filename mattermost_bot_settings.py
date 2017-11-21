b4d787d63321a42064244ff8c5dc8f0cf2037b47
TROUVERIE Joachim <jtrouverie@itrust.fr>
2017-11-21 08:36:51 +0100

    Base settings on environment vars

--- mattermost_bot_settings.py
+++ mattermost_bot_settings.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 
-BOT_URL = ""
-BOT_LOGIN = ""
-BOT_PASSWORD = ""
-BOT_TEAM = "ITrust"
+BOT_URL = os.environ.get("MATTERBOT_URL")
+BOT_LOGIN = os.environ.get("MATTERBOT_LOGIN")
+BOT_PASSWORD = os.environ.get("MATTERBOT_PASSWORD")
+BOT_TEAM = os.environ.get("MATTERBOT_TEAM")
+IGNORE_NOTIFIES = os.environ.get("MATTERBOT_IGNORE")
 
 PLUGINS = [
     "mattermost_bot.plugins",
