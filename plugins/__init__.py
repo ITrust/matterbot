303102f6ad16bfc3d6633beeb05e47e6b36ebae7
TROUVERIE Joachim <jtrouverie@itrust.fr>
2017-11-22 16:29:12 +0100

    Add ACL according to usernames for endpoints (fix #4)

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from functools import wraps
+
 
 MATTERMOST_MESSAGE_MAX_LENGTH = 4000
+
+
+def only_from_users(usernames):
+    """Restrict enpoint only for given user names"""
+    def wrapper(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            message = args[0]
+            username = message.get_username()
+            if username not in usernames:
+                message.send(
+                    "**Error** : You don't have access to this functionnality"
+                )
+            else:
+                return func(*args, **kwargs)
+        return wrapped
+    return wrapper
